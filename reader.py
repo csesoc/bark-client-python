032e8f0b175daa6224207e5f06c35c943199b088
Matthew Moss <mdm@cse.unsw.edu.au>
2013-03-01 09:14:57 -0800

    Refactor reader ready for threading

--- reader.py
+++ reader.py
@@ -12,36 +12,44 @@ TIMEOUT = 3
 NULL_READ = '---'
 LEADING_DATA = 'UID: '
 
-class USBSerialReader:
-    s = None
-    last_uid = ''
-
-    def __init__(self):
-        port_attempts = [SERIAL_PORT_BASE + str(x) for x in xrange(MAX_PORT_ATTEMPTS)]
-        for port in port_attempts:
-            if os.path.exists(port):
-                try:
-                    self.s = serial.Serial(port, BAUD_RATE, timeout=TIMEOUT)
-                    break
-                except serial.SerialException:
+def get_connection(verbose=False):
+    conn = None
+    port_attempts = [SERIAL_PORT_BASE + str(x) for x in xrange(MAX_PORT_ATTEMPTS)]
+    for port in port_attempts:
+        if os.path.exists(port):
+            try:
+                conn = serial.Serial(port, BAUD_RATE, timeout=TIMEOUT)
+                break
+            except serial.SerialException:
+                if verbose:
                     print >> sys.stderr, port + ' exists but is not readable'
                     print >> sys.stderr, 'Try chmod o+r ' + port
                     print >> sys.stderr, 'Ensure current user is in the \'dialout\' group'
-                    continue
+                continue
+    if not conn:
+        raise serial.SerialException
+    return conn
 
-        if not self.s:
+class USBSerialReader:
+    conn = None
+    last_uid = ''
+
+    def __init__(self):
+        try:
+            self.conn = get_connection(verbose=True)
+        except serial.SerialException:
             print >> sys.stderr, "No USB Serial devices found"
             sys.exit()
 
-    def read(self, timeout=10):
-        """Read a UID from the serial device, blocking until success"""
+    def read(self, timeout=None):
+        """Read a UID from the serial device, blocking until success or timeout"""
         start_time = time.time()
-        while (time.time() - start_time < timeout):
-            line = self.s.readline().strip()
+        while (timeout == None or time.time() - start_time < timeout):
+            line = self.conn.readline().strip()
             if NULL_READ not in line and LEADING_DATA in line and line != self.last_uid:
                 self.last_uid = line
                 break
-        return line[len(LEADING_DATA):]
+        return self.last_uid[len(LEADING_DATA):]
 
 if __name__ == "__main__":
     r = USBSerialReader()
